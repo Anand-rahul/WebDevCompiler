54e584c3b6d177d1ba5e02d85174ddee92d97c0f
Rahul Anand <anandrahul596@gmail.com>
2024-05-29 21:34:26 +0530

    Created basic frontend page for compiler

---
+++ src/pages/Compiler.tsx
@@ -0,0 +1,30 @@
+import CodeEditor from "@/components/CodeEditor";
+import HelperHeader from "@/components/HelperHeader";
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable";
+
+const Compiler = () => {
+  return (
+    <ResizablePanelGroup direction="horizontal">
+      <ResizablePanel
+        className="h-[calc(100dvh-60px)] min-w-[350px]"
+        defaultSize={50}
+      >
+        <HelperHeader />
+        <CodeEditor />
+      </ResizablePanel>
+      <ResizableHandle />
+      <ResizablePanel
+        className="h-[calc(100dvh-60px)] min-w-[350px]"
+        defaultSize={50}
+      >
+        Right Side
+      </ResizablePanel>
+    </ResizablePanelGroup>
+  );
+};
+
+export default Compiler;
